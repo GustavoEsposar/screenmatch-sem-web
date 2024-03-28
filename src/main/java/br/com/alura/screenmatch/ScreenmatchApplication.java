d4d85b258244627ddf697ed2f40e22d7027f0139
guesposar <guesposar@icloud.com>
2024-03-28 15:12:14 -0300

    adição de DadosEpsodio ao Model para os dados de cada epsodio da Serie

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.screenmatch.model.DadosEpsodio;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
@@ -20,13 +21,13 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		ConsumoApi consumoApi = new ConsumoApi();
 
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=7215ebcb&t=gilmore+girls");
-
-		//System.out.println(json);
-
 		ConverteDados cd = new ConverteDados();
 		DadosSerie dados = cd.obterDados(json, DadosSerie.class);
 
-		System.out.println(dados);
+		json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=7215ebcb&t=gilmore+girls&season=1&episode=1");
+		DadosEpsodio dadosEpsodio = cd.obterDados(json, DadosEpsodio.class);
+
+		System.out.println(dadosEpsodio);
 	}
 
 }
