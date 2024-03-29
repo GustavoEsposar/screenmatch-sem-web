ec67466bb6d5aec027fe49d78ad0b2daf7ec458f
guesposar <guesposar@icloud.com>
2024-03-29 18:15:17 -0300

    adicao de variavel de instancia enderecoPesquisa e correcao de seu uso

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -14,28 +14,26 @@ public class Principal {
     private Scanner leitura = new Scanner(System.in);
     private ConsumoApi consumoApi = new ConsumoApi();
     private ConverteDados cd = new ConverteDados();
+    private String enderecoPesquisa;
     private final String ENDERECO = "https://www.omdbapi.com/?apikey=7215ebcb";
 
     public void exibeMenu() {
         System.out.println("Digite o nome da série para busca: ");
         var nomeSerie = leitura.nextLine();
 
-		var json = consumoApi.obterDados(ENDERECO + "&t=" + nomeSerie.replace(" ", "+"));
-
+        enderecoPesquisa = ENDERECO + "&t=" + nomeSerie.replace(" ", "+");
+		var json = consumoApi.obterDados(enderecoPesquisa);
 		DadosSerie dados = cd.obterDados(json, DadosSerie.class);
-        System.out.println(dados);
+
         exibeTemporadas(json, dados);
     }
 
     private void exibeTemporadas(String json,DadosSerie dados) {
 		for(int i = 1; i < dados.totalTemporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?apikey=7215ebcb&t=gilmore+girls&season=" + i);
+			json = consumoApi.obterDados(enderecoPesquisa + "&season=" + i);
 			DadosTemporada dadosTemporada = cd.obterDados(json, DadosTemporada.class);
-            //dadosTemporada.episodios().forEach(System.out::println);
 			temporadas.add(dadosTemporada);
 		}
-		
         temporadas.forEach(System.out::println);
-        
     }
 }
